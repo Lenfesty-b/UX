abc92afe195b68363d7b67d9de89caea826ef465
Jamescunningham19 <43951234+Jamescunningham19@users.noreply.github.com>
2018-12-12 22:22:09 +0000

    We're fuckedddd

---
+++ UXcollab2/UX/Website/js/refGen.js
@@ -0,0 +1,13 @@
+// JavaScript source code
+
+window.onload = function () {
+
+    var bookingReference = Math.floor((Math.random() * 10000) + 1000);
+
+
+
+
+
+    document.getElementById('output').innerHTML = bookingReference;
+
+};
